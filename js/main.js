f7410dd75436ac8d3c7ceb5c8411a894f78fe3b2
Tori Keegan <toriakee@gmail.com>
2020-06-01 18:25:19 +0100

    Updates!

--- js/main.js
+++ js/main.js
@@ -162,12 +162,13 @@ jQuery(document).ready(function($) {
 				items: 1,
 				center: true,
 				loop: true,
-				margin: 0,
+				margin: 20,
 				autoplay: true,
 				nav: true,
 				dots: true,
 				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-				smartSpeed: 1000
+				smartSpeed: 1000,
+				autoWidth: true
 			});
 		}
 
